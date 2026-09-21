f53da1c2b66448dbf7024af9e41c9709fdcd3edb
Carlos Velasco <xtreemze@gmail.com>
2026-09-21 19:36:09 +0800

    chore: add playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: Boolean(process.env.CI),
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: process.env.CI ? 'github' : 'list',
+  use: {
+    baseURL: 'http://127.0.0.1:4173/booking/',
+    trace: 'retain-on-failure',
+  },
+  webServer: {
+    command: 'npm run build && npm run preview -- --host 127.0.0.1',
+    url: 'http://127.0.0.1:4173/booking/',
+    reuseExistingServer: !process.env.CI,
+  },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'mobile-chromium', use: { ...devices['Pixel 7'] } },
+  ],
+});
